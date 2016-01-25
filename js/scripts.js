2c410dea5325c80b482d78492461685e0a78cabf
Joshua Gustafson and Will Murphy <silversevenfox@yahoo.com>
2016-01-25 15:23:03 -0800

    add UI not finished

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,22 @@
-function ItemToDo(title,description) {
+function ItemToDo(title, description) {
   this.title = title;
   this.description = description;
 }
 
 $(document).ready(function() {
+  $("form#new-item").submit(function(event){
+      event.preventDefault();
+        var inputtedTitle = $("input#new-title").val();
+        var inputtedDescription = $("input#new-description").val();
+        var newItemToDo = new ItemToDo(inputtedTitle, inputtedDescription);
+      if (inputtedTitle === "") {
+        alert("Please fill out the form correctly!");
+      } else {
+        $("ol#toDoCol").append("<li><span class='item'>" + newItemToDo.title + " " + newItemToDo.description+ "</span></li>");
 
-  $("form").submit(function(event){
 
-    event.preventDefault();
+        $("input#new-title").val("");
+        $("input#new-description").val("");
+      }
   });
-
 });
